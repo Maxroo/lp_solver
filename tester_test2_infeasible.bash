c1299230383886750c0d18d001e9030199b8d388
Hao-Tse Wu <maxwuhz0624@gmail.com>
2022-07-16 11:31:57 -0700

    init tester bash file

---
+++ tester_test2_infeasible.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+python3 lp_solver.py < test_LPs_volume2/input/infeasible_3x3_1.txt
+python3 lp_solver.py < test_LPs_volume2/input/infeasible_3x3_2.txt
+python3 lp_solver.py < test_LPs_volume2/input/infeasible_3x3_3.txt
+python3 lp_solver.py < test_LPs_volume2/input/infeasible_3x3_4.txt
+python3 lp_solver.py < test_LPs_volume2/input/infeasible_3x3_5.txt
+python3 lp_solver.py < test_LPs_volume2/input/infeasible_10x7_1.txt
+python3 lp_solver.py < test_LPs_volume2/input/infeasible_10x7_2.txt
+python3 lp_solver.py < test_LPs_volume2/input/infeasible_10x7_3.txt
+python3 lp_solver.py < test_LPs_volume2/input/infeasible_10x7_4.txt
+python3 lp_solver.py < test_LPs_volume2/input/infeasible_10x7_5.txt
+
+
---
+++ tester_test2_optimal.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+python3 lp_solver.py < test_LPs_volume2/input/optimal_3x3_1.txt
+python3 lp_solver.py < test_LPs_volume2/input/optimal_3x3_2.txt
+python3 lp_solver.py < test_LPs_volume2/input/optimal_3x3_3.txt
+python3 lp_solver.py < test_LPs_volume2/input/optimal_3x3_4.txt
+python3 lp_solver.py < test_LPs_volume2/input/optimal_3x3_5.txt
+python3 lp_solver.py < test_LPs_volume2/input/optimal_3x3_6.txt
+python3 lp_solver.py < test_LPs_volume2/input/optimal_3x3_7.txt
+python3 lp_solver.py < test_LPs_volume2/input/optimal_3x3_8.txt
+python3 lp_solver.py < test_LPs_volume2/input/optimal_3x3_9.txt
+python3 lp_solver.py < test_LPs_volume2/input/optimal_10x7_1.txt
+python3 lp_solver.py < test_LPs_volume2/input/optimal_10x7_2.txt
+python3 lp_solver.py < test_LPs_volume2/input/optimal_10x7_3.txt
+python3 lp_solver.py < test_LPs_volume2/input/optimal_10x7_4.txt
+python3 lp_solver.py < test_LPs_volume2/input/optimal_10x7_5.txt
+python3 lp_solver.py < test_LPs_volume2/input/optimal_10x7_6.txt
+python3 lp_solver.py < test_LPs_volume2/input/optimal_10x7_7.txt
+python3 lp_solver.py < test_LPs_volume2/input/optimal_10x7_8.txt
+python3 lp_solver.py < test_LPs_volume2/input/optimal_10x7_9.txt
+
